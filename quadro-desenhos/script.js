8eb34f36a34b0f63db8b7cfa8cded335c4967e38
Cassia Maria <cassiamariaassis@gmail.com>
2021-08-07 19:45:42 -0300

    eventos no canvas

--- quadro-desenhos/script.js
+++ quadro-desenhos/script.js
@@ -1,11 +1,16 @@
 let currentColor = 'black';
+let canDraw = false;
 
-let screen = document.querySelector('#tela');
-let context = screen.getContext('2d');
+let screen = document.querySelector('#screen');
+let contextCanvas = screen.getContext('2d');
 
 document.querySelectorAll('.colorArea .color').forEach(item => {
     item.addEventListener('click', colorClickEvent);
 });
+
+screen.addEventListener('mousedown', mouseDownEvent);
+screen.addEventListener('mousemove', mouseMoveEvent);
+screen.addEventListener('mouseup', mouseUpEvent);
 
 function colorClickEvent(event) {
     const color = event.target.getAttribute('data-color');
@@ -15,3 +20,16 @@ function colorClickEvent(event) {
     event.target.classList.add('active');
 }
 
+function mouseDownEvent() {
+    canDraw = true;
+}
+
+function mouseMoveEvent(event) {
+    if(canDraw) {
+        console.log(event.pageX, event.pageY)
+    }
+}
+
+function mouseUpEvent() {
+    canDraw = false;
+}
