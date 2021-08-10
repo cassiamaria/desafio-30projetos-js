138a1d2473f52eb3db8e61ab9035c8e99c5b38ba
Cassia Maria <cassiamariaassis@gmail.com>
2021-08-09 21:09:36 -0300

    finalizando projeto de desenho com canvas

--- quadro-desenhos/script.js
+++ quadro-desenhos/script.js
@@ -1,35 +1,62 @@
-let currentColor = 'black';
+let currentColor = "black";
 let canDraw = false;
+let mouseX = 0;
+let mouseY = 0;
 
-let screen = document.querySelector('#screen');
-let contextCanvas = screen.getContext('2d');
+let screen = document.querySelector("#screen");
+let contextCanvas = screen.getContext("2d");
 
-document.querySelectorAll('.colorArea .color').forEach(item => {
-    item.addEventListener('click', colorClickEvent);
+document.querySelectorAll(".colorArea .color").forEach((item) => {
+  item.addEventListener("click", colorClickEvent);
 });
 
-screen.addEventListener('mousedown', mouseDownEvent);
-screen.addEventListener('mousemove', mouseMoveEvent);
-screen.addEventListener('mouseup', mouseUpEvent);
+screen.addEventListener("mousedown", mouseDownEvent);
+screen.addEventListener("mousemove", mouseMoveEvent);
+screen.addEventListener("mouseup", mouseUpEvent);
+document.querySelector(".clear").addEventListener("click", clearScreen);
 
 function colorClickEvent(event) {
-    const color = event.target.getAttribute('data-color');
-    currentColor = color;
+  const color = event.target.getAttribute("data-color");
+  currentColor = color;
 
-    document.querySelector('.color.active').classList.remove('active');
-    event.target.classList.add('active');
+  document.querySelector(".color.active").classList.remove("active");
+  event.target.classList.add("active");
 }
 
-function mouseDownEvent() {
-    canDraw = true;
+function mouseDownEvent(event) {
+  canDraw = true;
+  mouseX = event.pageX - screen.offsetLeft;
+  mouseY = event.pageY - screen.offsetTop;
 }
 
 function mouseMoveEvent(event) {
-    if(canDraw) {
-        console.log(event.pageX, event.pageY)
-    }
+  if (canDraw) {
+    drawing(event.pageX, event.pageY);
+  }
 }
 
 function mouseUpEvent() {
-    canDraw = false;
+  canDraw = false;
+}
+
+function drawing(x, y) {
+  let pointX = x - screen.offsetLeft;
+  let pointY = y - screen.offsetTop;
+
+  contextCanvas.beginPath();
+  contextCanvas.lineWidth = 5;
+  contextCanvas.lineJoin = "round";
+  contextCanvas.moveTo(mouseX, mouseY);
+  contextCanvas.lineTo(pointX, pointY);
+  contextCanvas.closePath();
+  contextCanvas.strokeStyle = currentColor;
+  contextCanvas.stroke();
+
+  mouseX = pointX;
+  mouseY = pointY;
+}
+
+function clearScreen() {
+  contextCanvas.setTransform(1, 0, 0, 1, 0, 0);
+  contextCanvas.clearRect(0, 0, contextCanvas.canvas.width, contextCanvas.canvas.height)
 }
