ce06d5ba2cb019b74a7e367c10e12b26dbd15a77
Cassia Maria <cassiamariaassis@gmail.com>
2021-08-14 00:26:12 -0300

    navbar style

---
+++ sidebar/assets/js/script.js
@@ -0,0 +1,24 @@
+const showMenu = (headerToggle, navbarId) => {
+    const toggleBtn = document.getElementById(headerToggle);
+    nav = document.getElementById(navbarId);
+
+    if (headerToggle && navbarId) {
+        toggleBtn.addEventListener('click', () => {
+            //Add the show-menu class to the div tag with the nav-menu class
+            nav.classList.toggle('show-menu');
+
+            //Change the icon
+            toggleBtn.classList.toggle('bx-x')
+        });
+    }
+};
+showMenu('header-toggle', 'navbar');
+
+const linkColor = document.querySelectorAll('.nav-link')
+
+function colorLink() {
+    linkColor.forEach(color = color.classList.remove('active'))
+    this.classList.add('active')
+}
+
+linkColor.forEach(color = color.classList.remove('click', colorLink))
